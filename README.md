20a07d617ce2d7311afe7ac8f704abf55ae9ae4f
dougUCN <douglas.kt.wong@gmail.com>
2019-02-26 17:38:24 -0500

    Working code, needs to be further stress tested for edge cases

---
+++ README.md
@@ -0,0 +1,69 @@
+qComsol  
+===============  
+Many universities/organizations have a comsol license with a limited number
+of maximum users at a given moment in time. This presents a problem if
+one attempts to submit jobs to a cluster, when one has no control over when
+the job starts running  
+
+qComsol submits a comsol job to a Torque queue, and resubmits it if it was
+rejected  
+
+Getting started
+--------------------
+Edit comsolJob.pbs so that -tmpdir and -recoverydir both point to
+an appropriate directory, and change the email address  
+
+Put qComsol.py, comsolJob.pbs, and check.pbs in the same directory
+as your comsol files, then simply run qComsol.py as described in the Usage section  
+
+Your completed comsol job will be a new file titled filename_solved.mph  
+
+
+Usage  
+----------------
+<pre>  
+
+usage: qComsol.py [-h] [-d DELAY] [-t TRIES] [-r] filename  
+
+submit comsol job to TORQUE system  
+
+positional arguments:  
+  filename              Accepts names w/ or w/o the '.mph' ending  
+
+optional arguments:  
+  -h, --help            show this help message and exit  
+  -d DELAY, --delay DELAY  
+                        Delay time for job resubmission [min]  
+  -t TRIES, --tries TRIES  
+                        Max # tries for resubmission  
+  -r, --removePBS       Removes PBS job submission scripts at the end of a run  
+
+</pre>
+
+comsolJob.pbs  
+----------------  
+Tells the torque queue to run the comsol file  
+
+Change settings here appropriately. Please note that $FILENAME, $TRIESLEFT, $DELAY,
+and $COMSOL_JOB are not actually environment variables. They are edited by qComsol.py  
+
+check.pbs  
+---------------  
+Note that the [--delay] flag in qComsol.py is limited by the wall time you set
+in this config file  
+
+Python version  
+------------------  
+This script is written for python 2. For compatibility with python 3 you'll
+need to comment out the import future print and change iteritems() to items()  
+
+Useful documentation on Torque and Comsol  
+-------------------------------------------
+*[qsub command](http://docs.adaptivecomputing.com/torque/4-0-2/Content/topics/commands/qsub.htm)*  
+*[Torque conditional execution](http://www.arc.ox.ac.uk/content/torque-job-scheduler#PBScondExecution)*  
+*[Comsol and torque submission](https://www.hpc.dtu.dk/?page_id=1257)*  
+
+Of particular note is the last link, where they mention that
+"the Comsol -np is used only to specify the processes that run on a single node ONLY"  
+
+For use of multiple nodes it seems that the intel-MPI library is necessary
---
+++ qComsol.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+template = "comsolJob.pbs"    # Name of the template submission file
+check = "check.pbs"             # Name of checking job script
+errorMessage = "Could not obtain license for COMSOL Multiphysics" # Error message to check for
+
+def main():
+    import argparse
+    import os.path
+    import time
+
+    parser = argparse.ArgumentParser(description='submit comsol job to TORQUE system')
+    parser.add_argument("filename", type=str, help="Accepts names w/ or w/o the '.mph' ending")
+    parser.add_argument("-d", "--delay", default=0, type=int, help="Delay time for job resubmission [min]")
+    parser.add_argument("-t", "--tries", default=0, type=int, help="Max # tries for resubmission")
+    parser.add_argument("-r", "--removePBS", action="store_true", help="Removes PBS job submission scripts")
+    parser.add_argument("-c", "--check", action="store_true")
+    args = parser.parse_args()
+
+    # args.filename should accept arguments both w/ and w/o '.mph'
+    filename = noExt(args.filename,".mph")
+
+    if (args.tries < 0):
+        print("Out of resubmission attempts")
+        return
+    else:
+        triesLeft = args.tries - 1
+
+    # args.check should be called only after the initial job has been run
+    if (args.check):
+        # Parse for error message
+        if errorMessage in open(filename+"_output.txt").read():
+            # Resubmit job if needed (with delay period)
+            print("License error message found")
+            time.sleep(args.delay * 60)
+            # remove output file to avoid rereading the same error message twice
+            os.remove( filename+"_output.txt" )
+            os.remove( filename+"_error.txt" )
+            print("Comsol job resubmitted\n")
+            submitToQueue(filename, template, check, args, triesLeft)
+            return
+
+        # If no error, quit
+        print("The comsol job ran without a license issue")
+        print("[resubmission attempts left: ", triesLeft, "]")
+        os.remove( filename+"_error.txt" )
+        os.remove( filename+"_check_error.txt" )
+        return
+
+    submitToQueue(filename, template, check, args, triesLeft)
+
+    return
+
+# Removes the extension name from a filename if present
+def noExt(filename, extensionName):
+    if filename[-len(extensionName):].find(extensionName) != -1:
+        return filename[:-len(extensionName)]
+    return filename
+
+# Creates "outfile". Words from "infile" are replaced according to the dictionary, "replacements"
+def copyReplace(inName, outName, replacements):
+    with open(inName) as infile, open(outName, 'w') as outfile:
+        for line in infile:
+            for src, target in replacements.iteritems():    #use iteritems() for python 2, items() for python 3
+                line = line.replace(src, target)
+            outfile.write(line)
+    return
+
+def submitToQueue(filename, template, check, args, triesLeft):
+    import os.path
+    import subprocess
+
+    # First check if required files exist
+    if not os.path.isfile(filename + ".mph"):
+        print("Error: Cannot find " + filename + ".mph")
+        return
+    if not os.path.isfile(template):
+        print("Error: Cannot find " + template)
+        return
+    if not os.path.isfile(check):
+        print("Error: Cannot find " + check)
+        return
+
+    # Make new file template_FILENAME.pbs
+    replacements = {'$FILENAME':filename}
+    comsolPBS = filename + ".pbs"
+    copyReplace(template, comsolPBS, replacements)
+
+    # Submit comsol job
+    # The last 4 characters are ".s1\n", I just want the job ID
+    comsolJob = subprocess.check_output(["qsub", comsolPBS])[:-4]
+
+    # Make new file check_FILENAME.pbs
+    replacements = {'$FILENAME':filename, '$TRIESLEFT':str(triesLeft), '$DELAY':str(args.delay), '$COMSOL_JOB':str(comsolJob)}
+    checkPBS = noExt(check,".pbs") + "_" + filename + ".pbs"
+    copyReplace(check, checkPBS, replacements)
+
+    # Submit checking job
+    checkingJob = subprocess.check_output(["qsub", checkPBS])[:-4]
+    print("Job numbers: ", comsolJob, ", ", checkingJob)
+
+    if args.removePBS:
+        print("Cleaning up PBS scripts")
+        os.remove( comsolPBS )
+        os.remove( checkPBS )
+    return
+
+if ( __name__ == '__main__' ):
+    main()
